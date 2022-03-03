39f7d3031bfac14535d25a531fe54781ab65db5b
amandamheredia <amanda.mnzh@gmail.com>
2022-03-03 10:43:22 -0300

    cambios css

---
+++ src/lib/firebase.js
@@ -0,0 +1,28 @@
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// import { firebaseConfig } from "./firebaseconfig";
+
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const auth = getAuth();
+
+// //Registra usuarios nuevos firebase
+
+// function register () {
+
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+//   })
+
+// }
--- src/main.js
+++ src/main.js
@@ -4,21 +4,25 @@
 
 myFunction(); */
 
+// import {register} from "./firebase"
+
 const header = document.getElementById("header");
-  const root = document.getElementById("root");
+const root = document.getElementById("root");
 
   let headerdiv = document.createElement("div")
   let logo = document.createElement("img")
-  let background = document.createElement("img")
+  let topFlower = document.createElement("img")
 
   headerdiv.setAttribute("class", "headerdiv")
   logo.setAttribute("src", "https://64.media.tumblr.com/9759038804c96b09f26666eda4ce9e5e/f80f47decba8e47b-06/s1280x1920/4cfe6992b169e9bd3d5c7fc7b388f798aaa9ab82.png")
   logo.setAttribute("class", "logo")
-  background.setAttribute("class", "background")
-  background.setAttribute("src", "https://64.media.tumblr.com/e18b8acf6151e278ddff601b9da7c75f/2c7d13e6f817310a-bb/s1280x1920/2c6334acc7b4a46ee4a60a60ef4e3cc60740536f.png")
+  topFlower.setAttribute("class", "topFlower")
+  topFlower.setAttribute("src", "https://64.media.tumblr.com/e18b8acf6151e278ddff601b9da7c75f/2c7d13e6f817310a-bb/s1280x1920/2c6334acc7b4a46ee4a60a60ef4e3cc60740536f.png")
+  
+  header.appendChild(topFlower)
   header.appendChild(headerdiv)
-  headerdiv.appendChild(background)
   headerdiv.appendChild(logo)
+  
 
   //
 
@@ -42,5 +46,5 @@ const header = document.getElementById("header");
   buttonregister.setAttribute("type", "submit")
   buttonregister.setAttribute("value", "Regístrate")
   buttonregister.setAttribute("class", "register")
-   formSignUp.appendChild(buttonregister)
+  formSignUp.appendChild(buttonregister)
   

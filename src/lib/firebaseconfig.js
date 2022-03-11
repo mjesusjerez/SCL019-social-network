452046b091dac89cf71c1bac8c97b6d11ba6e9d3
amandamheredia <amanda.mnzh@gmail.com>
2022-03-11 14:25:27 -0300

    avance diseño responsive

---
+++ src/lib/firebaseconfig.js
@@ -0,0 +1,9 @@
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyCLIzYilU7ayc3UC25Np9Dl5C7aSYFqmWw",
+//     authDomain: "saficas-rs.firebaseapp.com",
+//     projectId: "saficas-rs",
+//     storageBucket: "saficas-rs.appspot.com",
+//     messagingSenderId: "82522011082",
+//     appId: "1:82522011082:web:2fc656e5c9dc74d5d70b33",
+//     measurementId: "G-BQTQ93QXWM"
+//   };

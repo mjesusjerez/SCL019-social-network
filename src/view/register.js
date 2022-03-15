b4f5d047337303fe9da33a7457ded2984685ef84
amandamheredia <amanda.mnzh@gmail.com>
2022-03-15 08:58:02 -0300

    problemas css

--- src/view/register.js
+++ src/view/register.js
@@ -13,7 +13,7 @@ export const register = () => {
   
   headerdiv.setAttribute("class", "headerdiv")
   logo.setAttribute("src", "https://64.media.tumblr.com/9759038804c96b09f26666eda4ce9e5e/f80f47decba8e47b-06/s1280x1920/4cfe6992b169e9bd3d5c7fc7b388f798aaa9ab82.png")
-  logo.setAttribute("class", "logo")
+  logo.setAttribute("class", "logoregister")
   
   divregister.appendChild(headerdiv)
   headerdiv.appendChild(logo)
